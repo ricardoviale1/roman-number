a6f02584b96b4b8c12e2ff2d3d2ff147f88c8656
ricardoviale1 <ricardovialemolina@gmail.com>
2026-05-09 21:47:25 +0000

    feat: convertitore e printer per numeri da 1 a 100 (closes #11)

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -20,23 +20,23 @@ public class IntegerToRomanTest {
     }
 
     @Test
-    public void testConvertTwentyReturnsXX() {
-        assertEquals("XX", IntegerToRoman.convert(20));
-    }
-
-    @Test
-    public void testConvertFortyReturnsXL() {
-        assertEquals("XL", IntegerToRoman.convert(40));
-    }
-
-    @Test
     public void testConvertFiftyReturnsL() {
         assertEquals("L", IntegerToRoman.convert(50));
     }
 
+    @Test
+    public void testConvertNinetyReturnsXC() {
+        assertEquals("XC", IntegerToRoman.convert(90));
+    }
+
+    @Test
+    public void testConvertOneHundredReturnsC() {
+        assertEquals("C", IntegerToRoman.convert(100));
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void testConvertFiftyOneThrowsException() {
-        // Ora l'errore deve essere lanciato solo sopra 50!
-        IntegerToRoman.convert(51);
+    public void testConvertOneHundredAndOneThrowsException() {
+        // L'unico errore accettato ora è da 101 in su!
+        IntegerToRoman.convert(101);
     }
 }
